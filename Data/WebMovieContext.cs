c7950e53e640b501773f956c5e6bcfb36faed390
Casper <capper2704@hotmail.dk>
2023-08-21 10:43:15 +0200

    Nu virker lortet

---
+++ Data/WebMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebMovie.Models;
+
+namespace WebMovie.Data
+{
+    public class WebMovieContext : DbContext
+    {
+        public WebMovieContext (DbContextOptions<WebMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
